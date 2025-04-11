5c96305560f1ba9788ea84bbddaefd8cd80fa937
Maxwell <maxwell.wogsland@gmail.com>
2025-04-11 13:27:47 +0200

    json update

--- electrical_test.py
+++ electrical_test.py
@@ -2,12 +2,15 @@ import pyvisa
 import time
 import json
 
+# keihley manual:
+# https://www.artisantg.com/info/Keithley_486_Manual_202262105350.pdf?srsltid=AfmBOorqn0Pa1D8ihVX_Pn2nzS5twOQ0z1KIPOGxEPZcmO1HX3S42q9j
+
 # Static variables
 # This is so they can be easliy updated if needed
 # DO WE NEED NIVISA ANYMORE?? i htink for the drivers
 DAQ_ADDRESS = "USB0::0x2A8D::0x5101::MY58031367::0::INSTR"
 KEI_ADDRESS = "GPIB0::22::INSTR"
-DEFAULT_TIMEOUT = 10 # we dont use time here cuase we want to test 10 times before we fail it so it can build charge for some of them
+DEFAULT_TIMEOUT = 15 # we dont use time here cuase we want to test 10 times before we fail it so it can build charge for some of them
 # The Channel is used as the index
 LIMIT_LOW =  [None, None, 8000,  1000000, 1000000, 1000000, 1000000, None, None, 1000000, 1000000, 1000000, 90,  1000000, 1000000, 8000,  1000000, 1000000]
 LIMIT_HIGH = [2,    2,    12000, None,    None,    None,    None,    2,    2,    None,    None,    None,    110, None,    None,    15000, None,    None]
@@ -20,10 +23,16 @@ passed = True
 daq = None
 kei = None
 
+
+
 #BASSICLY IF THE RESULTS HAVE A NONE IN THEM THEN THAT MEANS SOMTHING IN THE MACHINE FAILED
 #We use 1 retry to speed things up cuase we will still try 10 times before we move on
 
 # A function to query the #daq and catch any erros so the whole program wont crash.
+def createJSON(data, filename):
+    with open(filename, "w") as json_file:
+        json.dump(data, json_file, indent=4)
+
 def safeReadDAQ(channel, retries=1):
     for _ in range(retries):
         try:
@@ -36,6 +45,7 @@ def safeReadDAQ(channel, retries=1):
     return None
 
 def safeReadKEI(retries=1):
+    #time.sleep(60)
     for _ in range(retries):
         try:
             res = kei.read()
@@ -103,10 +113,13 @@ def testCurrent(start, end):
 
     for v in range(start, end + step, step): #end + step??
         kei.write('V{},1,1X'.format(v))
+        kei.write('X')
         time.sleep(1)
 
         # DO WE NEED ALL THESE
         # CAN I SEND MULTIPLE COMMANDS AT ONCE
+
+    #time.sleep(30)
     kei.write('F0X')
     kei.write('B0X')
     kei.write('R0X')
@@ -140,13 +153,19 @@ def main():
     daq = rm.open_resource(DAQ_ADDRESS)
     kei = rm.open_resource(KEI_ADDRESS)
 
+    
+
     # DAQ configuration
     # MIGHT BE ABLE TO SEND ALL THESE AT ONCE
     daq.write("*RST")
     daq.timeout = 10000
-    daq.write("CONF:RES (@{})".format(range(101,119)))
+    #daq.write("CONF:RES (@{})".format(range(101,119)))
 
     # we can use list comprehention to make it faster as now we dont have to have an append call everytime can use more memory but propably not a problem
+
+    # kei.write('V400,1,1X')
+    # kei.write('X')
+
     resistance = [
         testChannelUpperLimit(c, u) if l is None else
         testChannelLowerLimit(c, l) if u is None else
@@ -154,10 +173,11 @@ def main():
         for c, l, u in zip(range(101,119), LIMIT_LOW, LIMIT_HIGH)
     ]
 
+    
     #Short all the channels on the DAQ
     #DOES THIS WORK?? and is it needed
-    #daq.write("ROUT:CLOS (@118)")
     daq.write("*RST")
+    daq.write("ROUT:CLOS (@101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118)")
 
     #Configure the kei 487 thang
     #DO WE NEED ALL THESE THINGS LIKE???
@@ -179,28 +199,37 @@ def main():
 
     #JSON Section
     #MIGHT NEED TO GO OVER THIS WITH SIMEN
-    result_data = {
+    ELRES_JSON = {
         "component": component,
-        "test": "electrical",
+        "test": "Electrical_RES",
         "institution": "UNIBERGEN",
         "date": date,
         "prefix": "",
         "passed": passed,
         "HI-LO": HI_LO,
-        "resistance": {
+        "RES": {
             "resistance": resistance,
             "limit-low": LIMIT_LOW,
             "limit-hight": LIMIT_HIGH,
-            "current": current,
-            "voltage": VOLTAGE
         }
     }
 
-    filename = "{}.json".format(component)
-    with open(filename, "w") as json_file:
-        json.dump(result_data, json_file, indent=4)
+    ELHV_JSON = {
+        "component": component,
+        "test": "Electrical_RES",
+        "institution": "UNIBERGEN",
+        "date": date,
+        "prefix": "",
+        "passed": passed,
+        "HV": {
+            "current": current,
+            "range": [-2e-07, 2e-07],
+            "voltage": [y for (x, y) in VOLTAGE]
+        }
+    }
 
+    createJSON(ELRES_JSON, "{}ELRES{}.json".format(component, date.split('T')[0]))
+    createJSON(ELHV_JSON, "{}ELHV{}.json".format(component, date.split('T')[0]))
     print("Time elapsed {}m {}s".format((int)((time.time() - start) / 60), round((time.time() - start) % 60, 2)))
-    print("Json file created with resistance for {} at {}".format(component, filename))
 
 if __name__=="__main__":main()
