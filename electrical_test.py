a15105c5acaf53ab4d3fd9cdfd66d737fd0db6cc
671419 <111427818+671419@users.noreply.github.com>
2025-05-01 13:28:32 +0200

    my dick fell off
    hehe

--- electrical_test.py
+++ electrical_test.py
@@ -4,21 +4,23 @@ import json
 
 # Static variables
 # This is so they can be easliy updated if needed
-# DO WE NEED NIVISA ANYMORE?? i htink for the drivers
 DAQ_ADDRESS = "USB0::0x2A8D::0x5101::MY58031367::0::INSTR"
 KEI_ADDRESS = "GPIB0::22::INSTR"
 DEFAULT_TIMEOUT = 15 # we dont use time here cuase we want to test 10 times before we fail it so it can build charge for some of them
 # The Channel is used as the index
 LIMIT_LOW =  [None, None, 8000,  1000000, 1000000, 1000000, 1000000, None, None, 1000000, 1000000, 1000000, 90,  1000000, 1000000, 8000,  1000000, 1000000]
 LIMIT_HIGH = [2,    2,    12000, None,    None,    None,    None,    2,    2,    None,    None,    None,    110, None,    None,    15000, None,    None]
+HV_LIMIT_LOW = -2e-7
+HV_LIMIT_HIGH = 2e-7
 # below is just for the json really
 HI_LO = ["VRET-VRET_S", "VRET-Vref", "VRET-HV_RET", "VRET-VIN", "VRET-CMD_N,GTXi_N", "VRET-NTC,LP_EN", "VRET-GND_C", "VIN-VIN_S", "VIN-Vsen", "VIN-CMD_N,GTXi_N", "VIN-NTC,LP_EN", "VIN-GND_C", "CMD_N,GTXi_N-CMD_P,GTXi_P", "CMD_N,GTXi_N-NTC,LP_EN", "CMD_N,GTXi_N-GND_C", "NTC,LP_EN-NTC_RET,MUX", "NTC,LP_EN-GND_C", "GND-GND_C"]
 VOLTAGE = [(0,-300), (-300, 0), (0, 300), (300, 0)]
 
 # Global vairabled
-passed = True
-daq = None
-kei = None
+passedRES = True
+passedHV = True
+daq = None #to control the daq
+kei = None #to control the keithley
 
 #BASSICLY IF THE RESULTS HAVE A NONE IN THEM THEN THAT MEANS SOMTHING IN THE MACHINE FAILED
 #We use 1 retry to speed things up cuase we will still try 10 times before we move on
@@ -47,82 +49,44 @@ def safeReadKEI(retries=1):
         except Exception as e:
             print("Error: {}, retrying...".format(e))
             time.sleep(1)
-            kei.clear() # is this possible or ?
     return None
 
-def testChannelLowerLimit(channel, lowerLimit, timeout=DEFAULT_TIMEOUT):
-    global passed
+
+
+def testChannel(channel, lowerLimit, upperLimit, timeout=DEFAULT_TIMEOUT):
+    global passedRES
 
     while (timeout):
         res = safeReadDAQ(channel)
 
         if (res is not None):
-            if (res > lowerLimit):
+            if ( (lowerLimit is None or lowerLimit < res) and (upperLimit is None or res < upperLimit) ):
                 print("Channel {} passed - {}ohms".format(channel, res))
                 return res
-                
+        time.sleep(0.1)
+        #does it apply voltage when we close switch or when we measure res?
         timeout -= 1
+        print("DEBUG: ___ timeout: {} res: {}".format(timeout, res))
 
-    passed = False
+    passedRES = False
     print("Channel {} failed - {}ohms".format(channel, res))
     return res
 
-def testChannelUpperLimit(channel, upperLimit, timeout=DEFAULT_TIMEOUT):
-    global passed
-
-    while(timeout):
-        res = safeReadDAQ(channel)
-
-        if (res is not None):
-            if (res < upperLimit):
-                print("Channel {} passed - {}ohms".format(channel, res))
-                return res
-                
-        timeout -= 1
-
-    passed = False
-    print("Channel {} failed - {}ohms".format(channel, res))
-    return res
-
-def testChannelBothLimits(channel, lowerLimit, upperLimit, timeout=DEFAULT_TIMEOUT):
-    global passed
-
-    while (timeout):
-        res = safeReadDAQ(channel)
-
-        if (res is not None):
-            if (lowerLimit < res and res < upperLimit):
-                print("Channel {} passed - {}ohms".format(channel, res))
-                return res
-                
-        timeout -= 1
-
-    passed = False
-    print("Channel {} failed - {}ohms".format(channel, res))
-    return res
 
 def testCurrent(start, end):
-    global passed
+    global passedHV
     step = -10 if end < start else 10
 
-    for v in range(start, end + step, step): #end + step??
+    for v in range(start, end + step, step): # Sweeps from current voltage to desired voltage
         kei.write('V{},1,1X'.format(v))
         time.sleep(1)
 
-        # DO WE NEED ALL THESE
-        # CAN I SEND MULTIPLE COMMANDS AT ONCE
-    kei.write('F0X')
-    kei.write('B0X')
-    kei.write('R0X')
-    kei.write('DX')
-
-    kei.write('T5X')
-
+        
     current = safeReadKEI()
 
-    if not (-2e-7 < current and current < 2e-7):
+    if not (HV_LIMIT_LOW < current and current < HV_LIMIT_HIGH):
         print("Voltage {}V -> {}V failed - {}A".format(start, end, current))
-        passed = False
+        passedHV = False
         return current
 
     print("Voltage {}V -> {}V passed - {}A".format(start, end, current))
@@ -147,13 +111,16 @@ def main():
     # MIGHT BE ABLE TO SEND ALL THESE AT ONCE
     daq.write("*RST")
     daq.timeout = 10000
-    daq.write("CONF:RES (@{})".format(range(101,119)))
+    #daq.write("CONF:RES (@{})".format(range(101,119)))
+
+    print(safeReadDAQ(108))
+
+    #for x in range(10):
+        #safeReadDAQ(104)
 
     # we can use list comprehention to make it faster as now we dont have to have an append call everytime can use more memory but propably not a problem
     resistance = [
-        testChannelUpperLimit(c, u) if l is None else
-        testChannelLowerLimit(c, l) if u is None else
-        testChannelBothLimits(c, l, u)
+        testChannel(c,l,u)
         for c, l, u in zip(range(101,119), LIMIT_LOW, LIMIT_HIGH)
     ]
 
@@ -167,52 +134,50 @@ def main():
     #With the write terminatino could i just have a big string and then have \n between each one??
     kei.write_termination = '\n'
     kei.read_termination = '\n'
-    kei.write('L0X')
-    kei.write('K0X')
-    kei.write('C0X')
-    kei.write('G0X')
-    kei.write('O1X') # turn on the flow i gueess can look at document later
+    kei.write('L0X\nK0X\nC0X\nG0X\nO1X')
 
     current = [testCurrent(s, e) for s, e in VOLTAGE]
 
     #turn off it sending power the keithley
 
+    kei.write('O0X')
     kei.close()
     daq.close()
 
-     #JSON Section
+    #JSON Section
     #MIGHT NEED TO GO OVER THIS WITH SIMEN
+    
     ELRES_JSON = {
         "component": component,
         "test": "Electrical_RES",
         "institution": "UNIBERGEN",
         "date": date,
         "prefix": "",
-        "passed": passed,
-        "HI-LO": HI_LO,
-        "RES": {
+        "passed": passedRES,
+        "results": {
+            "channel": HI_LO,
             "resistance": resistance,
             "limit-low": LIMIT_LOW,
-            "limit-hight": LIMIT_HIGH,
+            "limit-high": LIMIT_HIGH,
         }
     }
 
     ELHV_JSON = {
         "component": component,
-        "test": "Electrical_RES",
+        "test": "Electrical_HV",
         "institution": "UNIBERGEN",
         "date": date,
         "prefix": "",
-        "passed": passed,
-        "HV": {
+        "passed": passedHV,
+        "results": {
+            "voltage": [y for (x, y) in VOLTAGE],
             "current": current,
-            "range": [-2e-07, 2e-07],
-            "voltage": [y for (x, y) in VOLTAGE]
+            "limits": [HV_LIMIT_LOW, HV_LIMIT_HIGH]
         }
     }
 
-    createJSON(ELRES_JSON, "{}ELRES{}.json".format(component, date.split('T')[0]))
-    createJSON(ELHV_JSON, "{}ELHV{}.json".format(component, date.split('T')[0]))
+    createJSON(ELRES_JSON, "{}-resistance-{}.json".format(component, date.split('T')[0]))
+    createJSON(ELHV_JSON, "{}-highvoltage-{}.json".format(component, date.split('T')[0]))
     print("Time elapsed {}m {}s".format((int)((time.time() - start) / 60), round((time.time() - start) % 60, 2)))
 
 if __name__=="__main__":main()
